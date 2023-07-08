d680e904f80a4a08be4cff2a59745684313d5f0f
Vanja <69113324+vanjadu@users.noreply.github.com>
2023-07-08 02:38:07 +0200

    gitignore change

--- server/build/routes/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var AuthController_1 = require("../controllers/AuthController");
-var router = (0, express_1.Router)();
-// Auth routes
-var authPrefix = '/auth';
-router.post("".concat(authPrefix, "/register"), AuthController_1.register);
-exports.default = router;
