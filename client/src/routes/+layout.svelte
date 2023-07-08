4b3ca3197f2adf232acfdc4d4ed53c0ab88c5443
Vanja <69113324+vanjadu@users.noreply.github.com>
2023-07-08 02:34:06 +0200

    client clean slate

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Footer from '../components/Footer.svelte'
+  import Header from '../components/Header.svelte'
+  import '../styles/globals.css'
+</script>
+
+<svelte:head>
+  <title>
+    Beautiful Free Images & Pictures | Unsplash
+  </title>
+</svelte:head>
+
+<Header />
+<slot />
+<Footer />
